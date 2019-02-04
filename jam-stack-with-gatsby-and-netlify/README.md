718de07804655f48d31e4f7a1f4cdf14a72f4bca
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-02-04 17:03:13 +0200

    Test mdx-deck building

---
+++ jam-stack-with-gatsby-and-netlify/README.md
@@ -0,0 +1,1 @@
+# jam-stack-with-gatsby-and-netlify
---
+++ jam-stack-with-gatsby-and-netlify/index.js
@@ -0,0 +1,6 @@
+import Intro from './sections/01-intro.mdx'
+import Introduction from './sections/02-introduction.mdx'
+
+export { default as theme } from './theme'
+
+export default [...Intro, ...Introduction]
