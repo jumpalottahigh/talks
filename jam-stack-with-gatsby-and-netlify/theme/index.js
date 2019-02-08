1b80eeda27a1fd0bb566762bc816fea58c76708a
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-02-08 22:39:47 +0200

    Update copy

--- jam-stack-with-gatsby-and-netlify/theme/index.js
+++ jam-stack-with-gatsby-and-netlify/theme/index.js
@@ -1,12 +1,15 @@
-import { swiss as theme } from 'mdx-deck/themes'
+// import { big as theme } from 'mdx-deck/themes'
+// import { book as theme } from 'mdx-deck/themes' // Keep this as one lighter theme version
+// import { condensed as theme } from 'mdx-deck/themes' // Black background dark theme
+import { default as theme } from 'mdx-deck/themes'
 
 export default {
   ...theme,
   colors: {
-    ...theme.colors,
-    text: 'var(--text)',
-    heading: 'var(--black)',
-    link: 'var(--gatsby)',
+    ...theme.colors
+    // text: 'var(--text)',
+    // heading: 'var(--black)',
+    // link: 'var(--gatsby)'
   },
   link: {
     textDecoration: 'none'
@@ -16,12 +19,11 @@ export default {
     '@media screen and (min-width: 64em)': {
       fontSize: '32px'
     }
-  },
+  }
 
   // Customize your presentation theme here.
   //
   // Read the docs for more info:
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
 }
