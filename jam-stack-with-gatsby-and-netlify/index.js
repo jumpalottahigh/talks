73ef6a8c755b8977f6253aedff9807dfe6942394
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-02-09 00:30:30 +0200

    Work on slides

--- jam-stack-with-gatsby-and-netlify/index.js
+++ jam-stack-with-gatsby-and-netlify/index.js
@@ -1,6 +1,19 @@
 import Intro from './sections/01-intro.mdx'
 import Introduction from './sections/02-introduction.mdx'
+import Why from './sections/03-why.mdx'
+import Showcase from './sections/04-showcase.mdx'
+import Payoff from './sections/05-payoff.mdx'
+import Future from './sections/06-future.mdx'
+import Resources from './sections/07-resources.mdx'
 
 export { default as theme } from './theme'
 
-export default [...Intro, ...Introduction]
+export default [
+  ...Intro,
+  ...Introduction,
+  ...Why,
+  ...Showcase,
+  ...Payoff,
+  ...Future,
+  ...Resources
+]
