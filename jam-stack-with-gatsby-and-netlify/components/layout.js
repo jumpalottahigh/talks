16aa890ed01f7fe7e103dce7a355579b9c1b6cb9
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-02-10 23:39:55 +0200

    Work on slides

---
+++ jam-stack-with-gatsby-and-netlify/components/layout.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Layout = styled.div`
+  width: 100vw;
+  height: 100vw;
+  /* background-color: tomato; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Footer = styled.footer`
+  position: absolute;
+  width: 100%;
+  padding: 0 0.5rem;
+  bottom: 28px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const FooterLink = styled.a`
+  font-size: 1rem;
+  color: #07c;
+`
+
+export default ({ children }) => (
+  <Layout>
+    {children}
+    <Footer className="custom-layout__footer">
+      <FooterLink
+        className="footer__follow-along"
+        href="https://jam-stack-with-gatsby-and-netlify.netlify.com"
+      >
+        https://jam-stack-with-gatsby-and-netlify.netlify.com
+      </FooterLink>
+      <FooterLink
+        className="footer__twitter"
+        href="https://www.twitter.com/jumpalottahigh"
+      >
+        @jumpalottahigh
+      </FooterLink>
+    </Footer>
+  </Layout>
+)
--- jam-stack-with-gatsby-and-netlify/index.js
+++ jam-stack-with-gatsby-and-netlify/index.js
@@ -1,7 +1,7 @@
 import Intro from './sections/01-intro.mdx'
 import JAMstack from './sections/02-jamstack.mdx'
-import Why from './sections/03-why.mdx'
-import Showcase from './sections/04-showcase.mdx'
+import Netlify from './sections/03-netlify.mdx'
+import Gatsby from './sections/04-gatsby.mdx'
 import Payoff from './sections/05-payoff.mdx'
 import Future from './sections/06-future.mdx'
 import Resources from './sections/07-resources.mdx'
@@ -11,8 +11,8 @@ export { default as theme } from './theme'
 export default [
   ...Intro,
   ...JAMstack,
-  ...Why,
-  ...Showcase,
+  ...Netlify,
+  ...Gatsby,
   ...Payoff,
   ...Future,
   ...Resources
